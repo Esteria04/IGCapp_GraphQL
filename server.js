ac6bae9de5ec5100deca2929ab8a01ad1b0d96b7
Esteria04 <dev.esteria04@gmail.com>
2023-05-22 20:54:58 +0900

    .

--- server.js
+++ server.js
@@ -1,28 +1,29 @@
 import { ApolloServer, gql } from "apollo-server";
 
 const typeDef = gql`
+    type User {
+        id: ID,
+        name: String,
+        articles: [Article],
+        comments: [Comment]
+    }
+    type Article {
+        id: ID,
+        author: User,
+        publish-date: String,
+        modify-date: String,
+        title: String,
+        content: String,
+        comments: [Comment]
+    }
+    type Comment {
+        id: ID,
+        author: User,
+        article: Article,
+        content: String
+    }
     type Query {
-        type User {
-            id: ID,
-            name: String,
-            articles: [Article],
-            comments: [Comment]
-        }
-        type Article {
-            id: ID,
-            author: User,
-            publish-date: String,
-            modify-date: String,
-            title: String,
-            content: String,
-            comments: [Comment]
-        }
-        type Comment {
-            id: ID,
-            author: User,
-            article: Article,
-            content: String
-        }
+        
     }
 `;
 const server = new ApolloServer({ typeDef });
