f744efc964f2a21a290532f7121ad035855f38bb
Esteria04 <huijunyi0212@naver.com>
2023-05-27 13:42:23 +0900

    .

--- server.js
+++ server.js
@@ -1,32 +1,34 @@
 import { ApolloServer, gql } from "apollo-server";
 
-const typeDef = gql`
-    type User {
-        id: ID,
-        name: String,
-        articles: [Article],
-        comments: [Comment]
-    }
-    type Article {
-        id: ID,
-        author: User,
-        publish-date: String,
-        modify-date: String,
-        title: String,
-        content: String,
-        comments: [Comment]
-    }
-    type Comment {
-        id: ID,
-        author: User,
-        article: Article,
-        content: String
-    }
-    type Query {
-        
-    }
+const typeDefs = gql`
+  type User {
+    id: ID
+    name: String
+    articles: [Article]
+    comments: [Comment]
+  }
+  type Article {
+    id: ID
+    author: User
+    publishDate: String
+    modifyDate: String
+    title: String
+    content: String
+    comments: [Comment]
+  }
+  type Comment {
+    id: ID
+    author: User
+    article: Article
+    content: String
+  }
+  type Query {
+    users: [User]
+    articles: [Article]
+  }
 `;
-const server = new ApolloServer({ typeDef });
-server.listen().then(({url})=>{
-    console.log(`Running on ${url}`);
+const server = new ApolloServer({ typeDefs });
+server.listen().then(({ url }) => {
+  console.log(`Running on ${url}`);
 });
+   
