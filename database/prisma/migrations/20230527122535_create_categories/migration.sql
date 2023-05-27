5e6290f80b841b9c1d8e4ab268b37f2bdaf2d145
Esteria04 <huijunyi0212@naver.com>
2023-05-27 21:42:12 +0900

    .

---
+++ database/prisma/migrations/20230527122535_create_categories/migration.sql
@@ -0,0 +1,74 @@
+/*
+  Warnings:
+
+  - You are about to drop the `articles` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `comments` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `users` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropTable
+DROP TABLE `articles`;
+
+-- DropTable
+DROP TABLE `comments`;
+
+-- DropTable
+DROP TABLE `users`;
+
+-- CreateTable
+CREATE TABLE `User` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(191) NOT NULL,
+    `nickname` VARCHAR(191) NOT NULL,
+    `school` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Profile` (
+    `id` INTEGER NOT NULL,
+    `userId` INTEGER NOT NULL,
+
+    UNIQUE INDEX `Profile_userId_key`(`userId`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Comment` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `authorId` INTEGER NOT NULL,
+    `articleId` INTEGER NOT NULL,
+    `type` BOOLEAN NOT NULL,
+    `publishedDate` DATETIME(3) NOT NULL,
+    `modifiedDate` DATETIME(3) NOT NULL,
+    `content` VARCHAR(191) NOT NULL,
+
+    UNIQUE INDEX `Comment_authorId_key`(`authorId`),
+    UNIQUE INDEX `Comment_articleId_key`(`articleId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Article` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `authorId` INTEGER NOT NULL,
+    `type` BOOLEAN NOT NULL,
+    `publishedDate` DATETIME(3) NOT NULL,
+    `modifiedDate` DATETIME(3) NOT NULL,
+    `content` VARCHAR(191) NOT NULL,
+
+    UNIQUE INDEX `Article_authorId_key`(`authorId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Profile` ADD CONSTRAINT `Profile_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Comment` ADD CONSTRAINT `Comment_authorId_fkey` FOREIGN KEY (`authorId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Comment` ADD CONSTRAINT `Comment_articleId_fkey` FOREIGN KEY (`articleId`) REFERENCES `Article`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Article` ADD CONSTRAINT `Article_authorId_fkey` FOREIGN KEY (`authorId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
